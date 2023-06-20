8ed2db498925eb2878ca947e8285a2825bf11646
thuongnv <nvthuong11@gmail.com>
2023-06-21 04:59:55 +0700

    add loading for button when login and register

--- ShopeeClone/src/pages/Login/Login.tsx
+++ ShopeeClone/src/pages/Login/Login.tsx
@@ -9,6 +9,7 @@ import { isAxiosUnprocessableEntityError } from 'src/utils/utils'
 import { ErrorResponse } from 'src/types/utils.type'
 import { useContext } from 'react'
 import { AppContext } from 'src/contexts/app.context'
+import Button from 'src/components/Button'
 
 type FormData = Omit<Schema, 'confirm_password'>
 
@@ -83,9 +84,14 @@ function Login() {
                 autoComplete='on'
               />
               <div className='mt-3'>
-                <button className='w-full bg-red-500 px-2 py-4 text-center text-sm uppercase text-white hover:bg-red-600'>
+                <Button
+                  type='submit'
+                  className='flex  w-full items-center justify-center bg-red-500 px-2 py-4 text-sm uppercase text-white hover:bg-red-600'
+                  isLoading={loginAccountMutation.isLoading}
+                  disabled={loginAccountMutation.isLoading}
+                >
                   Đăng nhập
-                </button>
+                </Button>
               </div>
               <div className='mt-8 flex items-center justify-center'>
                 <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
