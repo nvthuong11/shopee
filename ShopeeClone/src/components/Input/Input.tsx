a47495b94929588c88b3433b1ed16b7eeda5b878
thuongnv <thuongnv@smartosc.com>
2023-06-23 17:26:26 +0700

    Update UI AsideFilter

--- ShopeeClone/src/components/Input/Input.tsx
+++ ShopeeClone/src/components/Input/Input.tsx
@@ -1,14 +1,12 @@
+import { InputHTMLAttributes } from 'react'
 import type { UseFormRegister, RegisterOptions } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  placeholder?: string
-  className?: string
-  name: string
-  register: UseFormRegister<any>
+  classNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
   rules?: RegisterOptions
-  autoComplete?: string
 }
 
 export default function Input({
@@ -19,8 +17,12 @@ export default function Input({
   autoComplete,
   name,
   register,
-  rules
+  rules,
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+  classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm'
 }: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
+
   return (
     <div className={className}>
       <input
@@ -28,9 +30,9 @@ export default function Input({
         className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
         placeholder={placeholder}
         autoComplete={autoComplete}
-        {...register(name, rules)}
+        {...registerResult}
       />
-      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
