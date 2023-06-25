748fb48d518a6f43bcabc0438a4e37e406190385
thuongnv <nvthuong11@gmail.com>
2023-06-25 08:28:21 +0700

    show data product when call api product list

--- ShopeeClone/src/pages/ProductList/Product/Product.tsx
+++ ShopeeClone/src/pages/ProductList/Product/Product.tsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom'
+import { Product as ProductType } from 'src/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
 
-export default function Product() {
+interface Props {
+  product: ProductType
+}
+
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%]'>
           <img
-            src='https://cf.shopee.vn/file/ea0f159f3f4c713abcf56b5ba73840b9_tn'
-            alt=''
+            src={product.image}
+            alt={product.name}
             className='absolute left-0 top-0 h-full w-full bg-white object-cover'
           />
         </div>
@@ -19,11 +25,13 @@ export default function Product() {
           <div className='mt-3 flex items-center'>
             <div className='max-w-[50%] truncate text-gray-500 line-through'>
               <span className='text-xs'>₫</span>
-              <span>5.000</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='ml-1 truncate text-orange'>
               <span className='text-xs'>₫</span>
-              <span>2.000</span>
+              <span>
+                <span>{formatCurrency(product.price)}</span>
+              </span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
@@ -62,7 +70,7 @@ export default function Product() {
               </div>
             </div>
             <div className='ml-2 text-sm'>
-              <span>5.66k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
