757bd90994e33d399917595800acf4dc1d75e1ad
thuongnv <thuongnv@smartosc.com>
2024-05-09 15:17:50 +0700

    Test App render và React Router - ERROR

---
+++ ShopeeClone/src/App.test.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import matchers from '@testing-library/jest-dom/matchers'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+expect.extend(matchers)
+
+describe('App', () => {
+  test('App render và chuyển page', async () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+  })
+})
--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -2,12 +2,26 @@ import { useEffect, useContext } from 'react'
 import { ToastContainer } from 'react-toastify'
 import useRouterElements from './useRouterElements'
 import { LocalStorageEventTarget } from './utils/auth'
-import { AppContext } from './contexts/app.context'
+import { AppContext, AppProvider } from './contexts/app.context'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ErrorBoundary from './components/ErrorBoundary'
+import { HelmetProvider } from 'react-helmet-async'
+
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
+
 function App() {
-  const routeElement = useRouterElements()
+  const routeElements = useRouterElements()
   const { reset } = useContext(AppContext)
 
   useEffect(() => {
@@ -18,10 +32,17 @@ function App() {
   }, [reset])
 
   return (
-    <div>
-      {routeElement}
-      <ToastContainer />
-    </div>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <ErrorBoundary>
+            {routeElements}
+            <ToastContainer />
+          </ErrorBoundary>
+        </AppProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HelmetProvider>
   )
 }
 
